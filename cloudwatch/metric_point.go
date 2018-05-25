2f1c0378fa51eec204e5c77d6c9bf9df606c8772
Brad Sickles <brad.sickles@gmail.com>
2018-05-25 13:08:02 -0400

    Pegging metric to time reported

--- cloudwatch/metric_point.go
+++ cloudwatch/metric_point.go
@@ -15,12 +15,14 @@ func CountMetricPoint(name string, count int) MetricPoint {
 	return &countMetricPoint{
 		name:  name,
 		count: count,
+		t:     time.Now(),
 	}
 }
 
 type countMetricPoint struct {
 	name  string
 	count int
+	t     time.Time
 }
 
 func (p *countMetricPoint) ToAWS() *cloudwatch.MetricDatum {
@@ -28,6 +30,6 @@ func (p *countMetricPoint) ToAWS() *cloudwatch.MetricDatum {
 		MetricName:        aws.String(p.name),
 		Value:             aws.Float64(float64(p.count)),
 		StorageResolution: aws.Int64(1),
-		Timestamp:         aws.Time(time.Now()),
+		Timestamp:         aws.Time(p.t),
 	}
 }
