bb343293cf37c4dba5b6ab658eb2f3b0bd0562c0
Brad Sickles <brad.sickles@gmail.com>
2018-05-25 13:07:43 -0400

    Batching metrics by 20

--- cloudwatch/put_metrics.go
+++ cloudwatch/put_metrics.go
@@ -8,20 +8,38 @@ import (
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
 )
 
+var (
+	maxMetricsPerPut = 20
+)
+
 func PutMetrics(namespace string, values []MetricPoint) error {
 	svc, err := services.NewCloudwatch()
 	if err != nil {
 		return fmt.Errorf("could not create cloudwatch service: %s", err)
 	}
 
-	data := make([]*cloudwatch.MetricDatum, 0)
+	batch := make([]*cloudwatch.MetricDatum, 0)
 	for _, item := range values {
-		data = append(data, item.ToAWS())
+		if len(batch) >= maxMetricsPerPut {
+			input := &cloudwatch.PutMetricDataInput{
+				Namespace:  aws.String(namespace),
+				MetricData: batch,
+			}
+			if _, err := svc.PutMetricData(input); err != nil {
+				return err
+			}
+			batch = []*cloudwatch.MetricDatum{item.ToAWS()}
+		} else {
+			batch = append(batch, item.ToAWS())
+		}
 	}
-	input := &cloudwatch.PutMetricDataInput{
-		Namespace:  aws.String(namespace),
-		MetricData: data,
+	if len(batch) > 0 {
+		input := &cloudwatch.PutMetricDataInput{
+			Namespace:  aws.String(namespace),
+			MetricData: batch,
+		}
+		_, err := svc.PutMetricData(input)
+		return err
 	}
-	_, err = svc.PutMetricData(input)
-	return err
+	return nil
 }
