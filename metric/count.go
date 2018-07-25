4e61d2da356da412b807ef415b796e32bc821a8c
Brad Sickles <brad.sickles@gmail.com>
2018-07-25 10:08:03 -0400

    Updating metric Count to work seamlessly with Add

--- metric/count.go
+++ metric/count.go
@@ -4,8 +4,8 @@ import (
 	"time"
 )
 
-func Count(name string, count int, tags map[string]string) Point {
-	return Point{
+func Count(name string, count int, tags map[string]string) *Point {
+	return &Point{
 		Metric:    name,
 		Unit:      "Count",
 		Timestamp: time.Now(),
